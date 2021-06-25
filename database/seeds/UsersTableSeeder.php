55d78d85edf2cd3866d21783f27de37f81b8cc9d
Husnain Ahmad <husnaincheema8080@gmail.com>
2021-06-25 18:51:58 +0500

    v

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,7 +17,18 @@ class UsersTableSeeder extends Seeder
                 'created_at'     => '2019-09-15 06:09:29',
                 'updated_at'     => '2019-09-15 06:09:29',
             ],
-        ];
+			   [
+                'id'             => 2,
+                'name'           => 'HUSNAIN AHMAD',
+                'email'          => 'husnain@gmail.com',
+                'password'       => '$2y$10$FW.q4Ajr04FAMX6EoB21G.mhuf/Qrd7XLBdQL/Q7GjI4WU3fuMHpm',
+                'remember_token' => null,
+                'created_at'     => '2021-06-25 08:24:47',
+                'updated_at'     => '2021-06-25 08:24:47',
+            ],
+			   
+			   
+			   ];
 
         User::insert($users);
     }
