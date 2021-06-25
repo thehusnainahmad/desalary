b2c98802ca90eeae32326bf7275267ec81d83faa
Husnain14 <42809521+Husnain14@users.noreply.github.com>
2021-06-25 17:56:54 +0500

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,7 +12,7 @@ class UsersTableSeeder extends Seeder
                 'id'             => 1,
                 'name'           => 'Admin',
                 'email'          => 'admin@admin.com',
-                'password'       => '1122',
+                'password'       => '$2y$10$rA.UbFkvBY0TnPXC5AvdEew7ITCs.PFo9eAfkH.m48GlOdhFMXvLG',
                 'remember_token' => null,
                 'created_at'     => '2019-09-15 06:09:29',
                 'updated_at'     => '2019-09-15 06:09:29',
